cddaa576edf0b22bca52ddf9960dd4ce36ae70cd
Philippa <p.giibwa@alustudent.com>
2023-09-04 12:18:53 -0700

    Task 0

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,24 +1,2 @@
 #!/usr/bin/env ruby
-if ARGV.empty?
-  puts "Usage: ruby match_school.rb <input_string>"
-  exit(1)
-end
-
-# Get the input string from the command line argument
-input_string = ARGV[0]
-
-# Define a regular expression to match "School"
-regex = /School/
-
-# Use the regular expression to find the first match in the input string
-match = input_string.match(regex)
-
-# Check if there is a match
-if match
-  matched_text = match[0]
-  match_length = matched_text.length
-  puts "#{matched_text}"
-  puts "(#{match_length} chars long)"
-else
-  puts "No match found."
-end
+puts ARGV[0].scan(/School/).join
