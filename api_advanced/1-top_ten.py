ef1356ce70eaa79f3afa6886275c07469bb8563b
Philippa Giibwa <p.giibwa@alustudent.com>
2023-11-08 19:37:06 +0000

    Task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ import requests
 
 def top_ten(subreddit):
     """"top ten"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10 \
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
         .format(subreddit)
 
     res = requests.get(url,
