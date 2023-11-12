62798d128c7429bf3a2c084911b28be56d922c63
Philippa Giibwa <p.giibwa@alustudent.com>
2023-11-12 21:47:42 +0000

    Task 2

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ a recursive function that queries the Reddit API """
-import requests 
+import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
@@ -13,12 +13,12 @@ def recurse(subreddit, hot_list=[], after=None):
                                 headers=headers,
                                 params=params,
                                 allow_redirects=False
-                           )
+                            )
     if response.status_code == 200:
         data = response.json().get('data')
         if data is not None:
             children = data.get('children')
-            if children is not None: 
+            if children is not None:
                 for child in children:
                     hot_list.append(child.get('data').get('title'))
                 after = data.get('after')
